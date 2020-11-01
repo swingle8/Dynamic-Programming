38884c18bd6349e713d5956f07e50ecea5f49391
Shivam Singhal <swingle8@gmail.com>
2020-11-01 11:14:38 +0530

    Update Unique Paths.java

--- Unique Paths.java
+++ Unique Paths.java
@@ -1,3 +1,4 @@
+//https://leetcode.com/problems/unique-paths/
 class Solution {
     public int uniquePaths(int m, int n) {
         int [][] dp = new int[m+1][n+1];
@@ -8,5 +9,19 @@ class Solution {
             }
         }
         return dp[m][n];
+    }
+}
+
+//the below code for the above problem saves O(n) space
+class Solution {
+    public int uniquePaths(int m, int n) {
+        int [] dp = new int[n+1];
+        dp[1] = 1;
+        for (int i = 1 ; i <= m ; i++) {
+            for (int j = 1 ; j <= n ; j ++) {
+                dp[j] = dp[j] + dp[j-1];
+            }
+        }
+        return dp[n];
     }
 }
