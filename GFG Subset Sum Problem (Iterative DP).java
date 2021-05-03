5e5195b2f22c684708820fc65ad4ca834fa7a629
Shivam Singhal <swingle8@gmail.com>
2021-05-03 15:34:08 +0530

    Add files via upload

--- GFG Subset Sum Problem (Iterative DP).java
+++ GFG Subset Sum Problem (Iterative DP).java
@@ -1,55 +1,58 @@
 // { Driver Code Starts
-import java.io.*;
-import java.util.*;
-
-class GFG
-{
-    public static void main(String args[])throws IOException
+    import java.io.*;
+    import java.util.*;
+    
+    class GFG
     {
-        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(read.readLine());
-        while(t-- > 0)
+        public static void main(String args[])throws IOException
         {
-            int N = Integer.parseInt(read.readLine());
-            String input_line[] = read.readLine().trim().split("\\s+");
-            int arr[]= new int[N];
-            for(int i = 0; i < N; i++)
-                arr[i] = Integer.parseInt(input_line[i]);
-            int sum = Integer.parseInt(read.readLine());
-
-            Solution ob = new Solution();
-            if(ob.isSubsetSum(N, arr, sum))
-            System.out.println(1);
-            else
-            System.out.println(0);
-
-            
-        }
-    }
-}
-// } Driver Code Ends
-
-
-//User function Template for Java
-
-class Solution{
-
-
-    static Boolean isSubsetSum(int N, int arr[], int sum){
-        int [][] dp = new int[arr.length+1][sum+1];
-        //int calculatedSum = hasSum (arr, sum, N-1, dp);
-        for (int i = 0 ; i <= arr.length; i++) {
-            for (int j = 0 ; j <= sum ; j++) {
-                if (i == 0 || j == 0)
-                    dp[i][j] = 0;
-                else if (j < arr[i-1])
-                    dp[i][j] = dp[i-1][j];
-                else {
-                    dp[i][j] = Math.max(dp[i-1][j], arr[i-1] + dp[i-1][j-arr[i-1]]);
-                }
+            BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+            int t = Integer.parseInt(read.readLine());
+            while(t-- > 0)
+            {
+                int N = Integer.parseInt(read.readLine());
+                String input_line[] = read.readLine().trim().split("\\s+");
+                int arr[]= new int[N];
+                for(int i = 0; i < N; i++)
+                    arr[i] = Integer.parseInt(input_line[i]);
+                int sum = Integer.parseInt(read.readLine());
+    
+                Solution ob = new Solution();
+                if(ob.isSubsetSum(N, arr, sum))
+                System.out.println(1);
+                else
+                System.out.println(0);
+    
+                
             }
         }
-        
-        return dp[arr.length][sum] == sum;
     }
-}
+    // } Driver Code Ends
+    
+    
+    //User function Template for Java
+    
+    class Solution{
+    
+    
+        static Boolean isSubsetSum(int N, int arr[], int sum){
+            boolean [][] dp = new boolean[arr.length+1][sum+1];
+            //int calculatedSum = hasSum (arr, sum, N-1, dp);
+            dp[0][0] = true;
+            for (int i = 0 ; i <= arr.length; i++) {
+                for (int j = 0 ; j <= sum ; j++) {
+                    if (i == 0 && j != 0)
+                        dp[i][j] = false;
+                    else if (j == 0)
+                        dp[i][j] = true;
+                    else if (j < arr[i-1])
+                        dp[i][j] = dp[i-1][j];
+                    else {
+                        dp[i][j] = dp[i-1][j] ||  dp[i-1][j-arr[i-1]];
+                    }
+                }
+            }
+            
+            return dp[arr.length][sum];
+        }
+    }
